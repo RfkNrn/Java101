7a1433b459fd94a07437c7933e898bb087c7183c
Refik <refik.onaran@gmail.com>
2026-07-06 21:20:33 +0300

    evenCounter.java modified

--- evenCounter.java
+++ evenCounter.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class evenCounter {
+    static void main() {
+        int counter;
+        Scanner input =  new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        counter = input.nextInt();
+        input.close();
+
+        for (int i = 1; i <= counter; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
